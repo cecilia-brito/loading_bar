c29cbf74d0662a7ea3dbf3decf0bc7db7d87dd4d
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2022-11-12 15:03:07 -0300

    refatoração do códico, mudança do botão de reset, add icon

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
-const btnStart = document.getElementById('btn');
+const btnStart = document.getElementById('btn_start');
 const btnReset = document.getElementById('reset');
 const bar = document.getElementById('bar');
 const elemPercentage = document.getElementById('percentage');
 
-let barWidth = 1;
-  
+let barWidth = 0;
+let percentage;
+
 function loadingBar(){
-    if(barWidth < 90){
+    if(barWidth <= 60){
+        console.log("funcão loading, estou funcionando")
+        barWidth += 9;
+        bar.style.width = barWidth +'%';
 
-        barWidth+=9;
-        bar.style.width= width+'%';
+        percentage = Math.round((barWidth * 100)/100);
+        elemPercentage.textContent = percentage + '%';
+        console.log(barWidth)
+    } else if(barWidth > 50 && barWidth < 90){
+        console.log("funcão loading, estou funcionando")
+        barWidth += 1;
+        bar.style.width = barWidth +'%';
 
-        let percentage = Math.round((barWidth*100)/100);
-        elemPercentage.textContent = percentage + 9 + '%';
+        percentage = Math.round((barWidth * 100)/100);
+        elemPercentage.textContent = percentage + 10 + '%';
+        console.log(barWidth)
 
-        if(barWidth == 90){
-            btnStart.style.display = 'none'
+    } else {
             btnReset.style.display = 'block'
+            console.log("funcão loading, terminei")
             return;
-        }  
     }
 }
 
 function moveBar(){
-    setInterval(loadingBar, 100);
+    for(let i = 0; i < 35; i++){
+        setTimeout(loadingBar, 1000)
+    }
+}
+
+function clearBar(){
+    console.log("funcão clear, estou funcionando")
+    barWidth = 0;
+    for(let i = parseInt(elemPercentage.textContent); i >= 0; i--){
+        elemPercentage.textContent = i + "%";
+        bar.style.width = i + "%";
+    }
+    btnReset.style.display = 'none';
+    console.log("funcção clear, terminei")
 }
 
 btnStart.addEventListener('click', moveBar);
+btnReset.addEventListener('click', clearBar);
